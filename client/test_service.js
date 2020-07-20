cb1f8adf753c5df7da09c1641cccd340a64ff3b6
Pete Wall pwall@pivotal.io <pwall@pivotal.io>
2020-07-20 16:50:10 -0500

    Move files into a data directory

--- client/test_service.js
+++ client/test_service.js
@@ -3,13 +3,9 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
-const io = require('socket.io')(http)
+require('socket.io')(http)
+
+app.use('/', express.static('../data'))
+
 const port = 3000
-
-// io.on('connection', (socket) => {
-//     console.log('Client connected!')
-//   socket.join(['server-updates', 'sensor-updates'])
-// })
-
-app.use('/', express.static('.'))
 http.listen(port, () => console.log(`Test service running at http://localhost:${port}`))
